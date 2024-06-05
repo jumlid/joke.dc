40d855a4f25147704789b8e6cc16bc0a7eae3f36
jumli_d <148221429+jumli404@users.noreply.github.com>
2024-06-05 19:10:55 +0530

    upload

---
+++ joke.js
@@ -0,0 +1,48 @@
+let prefix = './'
+let l;
+let fetch = require('node-fetch')
+let q
+let url = 'https://v2.jokeapi.dev/joke/Any'
+let token= require('./user.json')
+
+const { Client, GatewayIntentBits, Guild } = require('discord.js');
+const { datacatalog } = require('googleapis/build/src/apis/datacatalog');
+let client = new Client({
+    intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+})
+client.once('ready', () => {
+    console.log('-------------')
+    console.log('ready')
+    console.log('-------------')
+  
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content.startsWith(prefix)) {
+        if (message.content != "") {
+            console.log(message.content)
+        }
+        if (message.content.startsWith(prefix + 'play')) {
+            joke()
+            function joke() {
+
+
+                fetch(url)
+                    .then(res => res.json())
+                    .then(data => {
+                        message.channel.send(data.setup),
+                            message.channel.sendTyping()
+                        setTimeout(() => {
+                            message.channel.send(data.delivery)
+                        }, 3000)
+
+                    }
+                    )
+            }
+        }
+    }
+    else {
+        console.error(message.content)
+    }
+})
+client.login(token[0].token)
