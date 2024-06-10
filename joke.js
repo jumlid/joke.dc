3317470a17da507f2a02f591c1aeb7745e8bc690
jumli_d <148221429+jumli404@users.noreply.github.com>
2024-06-10 21:26:51 +0530

    Update joke.js

--- joke.js
+++ joke.js
@@ -1,45 +1,90 @@
-let prefix = './'
-let fetch = require('node-fetch')
 let url = 'https://v2.jokeapi.dev/joke/Any'
-let token= require('./user.json')
-
-const { Client, GatewayIntentBits, Guild } = require('discord.js');
-const { datacatalog } = require('googleapis/build/src/apis/datacatalog');
-let client = new Client({
-    intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
+let prefix = "!joke"
+let { Client, GatewayIntentBits, Message } = require('discord.js')
+let c = new Client({
+    intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageTyping
+    ]
 })
-client.once('ready', () => {
-    console.log('-------------')
-    console.log('ready')
-    console.log('-------------')
-  
+let channelid = ""/enter your channel ID/
+let channel;
+
+
+
+c.once('ready', () => {
+    console.log('                          ')
+    console.log('          ready           ')
+    console.log('                          ')
+
+    setInterval(() => {
+        checks(joke)
+
+    }, 3600000);
+   
+
+
+
 })
 
-client.on('messageCreate', (message) => {
-    if (message.content.startsWith(prefix)) {
-        if (message.content != "") {
-            console.log(message.content)
-        }
-        if (message.content.startsWith(prefix + 'play')) {
-          
+function checks(joke) {
+    channel = c.channels.cache.get(channelid)
+
+    if (channel) {
+        console.log('hi')
 
 
-                fetch(url)
-                    .then(res => res.json())
-                    .then(data => {
-                        message.channel.send(data.setup),
-                            message.channel.sendTyping()
-                        setTimeout(() => {
-                            message.channel.send(data.delivery)
-                        }, 3000)
-
-                    }
-                    )
-            
-        }
     }
-    else {
-        console.error(message.content)
+
+    if (!channel) {
+        console.log('error')
     }
+    joke()
+   
+
+}
+
+function joke() {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            if (data.setup || data.delivery != "") {
+                console.log(`${(data.setup.trim())}`)
+              
+                channel.send(data.setup.trim())
+                setTimeout(() => {
+                    channel.send('‎ ‎ ')
+                    channel.send('‎ ‎ ')
+
+                    console.log(`${data.delivery.trim()}`)
+                    channel.send(data.delivery.trim())
+                    channel.send('‎ ‎ ')
+                }, 2000)
+            }
+            else {
+                console.log('joke 404')
+                joke()
+            }
+
+        })
+        .catch(Error => {
+            console.log('API error')
+            checks(joke)
+        })
+
+
+    
+
+}
+
+
+c.on('messageCreate', m => {
+    if (m.content.startsWith(prefix)) {
+        console.log(`cammand by ${m.author.username} `)
+        checks(joke)
+    }
+
 })
-client.login(token[0].token)
+
+
+
+
+c.login('token')
