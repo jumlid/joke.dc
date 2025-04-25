fdfe33e6ab08566798d20a905333abea13becf18
boi <148221429+jumlid@users.noreply.github.com>
2025-04-25 15:59:45 +0530

    Fixed some major bugs and added some features
    Fixed 👍🏼 : Now bot can store different cammands on a file different file name data.json
    feature : Now bot can send joke when users type sudo joke

--- joke.js
+++ joke.js
@@ -1,9 +1,7 @@
-
 const { Client, GatewayIntentBits, Guild } = require('discord.js')
 const fs = require('fs')
 const { parse } = require('path')
 const c = new Client({ intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] })
-
 let steve_memory = {
     // This is just a temp memory it will hold users temp data like temp settings and stuff untill users tyoe cammand to save
 }
@@ -12,8 +10,11 @@ let steve = {
     "cammands": {
         "add": "Used to add new genre",
         "status": "used to check all the active genre",
-        "start": "use to start the service",
-        "help": "read all the available cammands"
+        "save": "to save all the changes",
+        "help": "read all the available cammands",
+        "joke": "To send joke",
+        "start": "To start the timer"
+
     },
     "category": {
         "Programming": false,
@@ -64,13 +65,34 @@ c.on('messageCreate', (m) => {
 
             }
             else {
-                // This is same as above i used two of them because in case of a server restart i'll lose the temp data so this will maintain my structure hehe
-                steve_memory[m.guild.id] = {
-                    "name": m.guild.name,
-                    "id": m.guild.id,
-                    "category": { ...steve.category },// Here i used spread oprator since js stored non premitive data as refrence 
+                let older_Data = fs.readFileSync('./data.json', 'utf8') // This is to restore the data lose from the bot restart and stuff
+                older_Data = JSON.parse(older_Data)
+                if (m.guild.id in older_Data) {
+                    steve_memory[m.guild.id] = older_Data[m.guild.id]
+                }
+                else {
+                    // This is same as above i used two of them because in case of a server restart i'll lose the temp data so this will maintain my structure hehe
+
+                    steve_memory[m.guild.id] = {
+                        "name": m.guild.name,
+                        "id": m.guild.id,
+                        "category": { ...steve.category },// Here i used spread oprator since js stored non premitive data as refrence 
+
+                    }
 
                 }
+
+
+
+
+
+
+
+
+
+
+
+
 
             }
 
@@ -80,41 +102,46 @@ c.on('messageCreate', (m) => {
 
 
             if (m.content.split(' ')[1] in steve.cammands) {
-                let o = fs.readFileSync('./data.json', 'utf8') // This is to restore the data lose from the bot restart and stuff
-                o = JSON.parse(o)
-                if (m.guild.id in o) {
-                    steve_memory[m.guild.id] = o[m.guild.id]
 
-                }
+                console.log(steve_memory[m.guild.id])
                 //   console.log(steve_memory)
 
                 switch (m.content.split(' ')[1]) {
                     case "add":
-                        if (JSON.parse(m.content.split(' ')[3]) === true || JSON.parse(m.content.split(' ')[3]) === false) {
+                        if (m.content.split(' ')[2] in steve.category) {
+                            if (JSON.parse(m.content.split(' ')[3]) === true || JSON.parse(m.content.split(' ')[3]) === false) {
 
-                            steve_memory[m.guild.id].category[m.content.split(' ')[2]] = JSON.parse(m.content.split(' ')[3])
-                            console.log(steve_memory)
-                            m.channel.send(`Chaning steve setting category ${m.content.split(' ')[2]} to ${m.content.split(' ')[3]}  `)
+                                steve_memory[m.guild.id].category[m.content.split(' ')[2]] = JSON.parse(m.content.split(' ')[3])
+                                console.log('fir', steve_memory)
+                                m.channel.send(`Chaning steve setting category ${m.content.split(' ')[2]} to ${m.content.split(' ')[3]}  `)
 
 
+                            }
+                            else {
+                                m.channel.send(`${m.content.split(' ')[3]} isn't a boolean value a boolean value can be either true(on) or false(off)`)
+                            }
                         }
                         else {
-                            m.channel.send(`${m.content.split(' ')[3]} isn't a boolean value a boolean value can be either true(on) or false(off)`)
+                            m.channel.send(`${m.content.split(' ')[2]} isn't a valid category please check the spelling or check the case(uppercase/lowercase) by cammand sudo status`)
                         }
+
                         break;
                     case "status":
-
+                        //       console.log('second', steve_memory[m.guild.id].category)
                         m.channel.send(JSON.stringify(steve_memory[m.guild.id].category))
 
 
                         break;
-                    case "start":
+                    case "save":
 
 
                         save(m.guild.id)
                         break;
                     case "help":
                         m.channel.send('1) sudo status: to view all the available category \n 2) sudo add <category> <true/false> to change the  joke according to your liking \n 3) sudo start to save all the previous cammand and to start the bot ')
+                        break;
+                    case "joke":
+                        joke(steve_memory[m.guild.id].url)
                         break;
 
                     default:
@@ -136,9 +163,22 @@ c.on('messageCreate', (m) => {
         older_message = fs.readFileSync('./data.json', 'utf8')
         older_message = JSON.parse(older_message)
         older_message[id] = steve_memory[id]
+        older_message[m.guild.id].url_a = []
+        Object.entries(older_message[id].category).forEach(e => {
+            // console.log(e[1])
+            if (e[1]) {
 
+                older_message[m.guild.id].url_a.push(e[0])
+                console.log(e[0])
+            }
 
+        })
+        older_message[m.guild.id].url = older_message[m.guild.id].url_a.join(',')
+        if (url.split(',').length < 4) {
+            m.channel.send('please select atleast 4 category else you may face API error ')
 
+        }
+        //   joke(older_message[m.guild.id].url)
 
         m.channel.send('setting saved ')
 
@@ -146,9 +186,39 @@ c.on('messageCreate', (m) => {
         fs.writeFileSync('./data.json', older_message)
         console.log('already')
     }
+    function joke(url) {
 
+        let play = false
+        console.log(url.split(',').length)
+
+
+        if (url.split(',').length === 1) {
+            play = true
+            url = 'any'
+            console.log('a', url)
+        }
+        if (play) {
+
+
+
+            fetch(`https://v2.jokeapi.dev/joke/${url}`)
+                .then(res => res.json())
+                .then(data => {
+                    m.channel.send(data.setup.trim())
+                    if (data.delivery.trim() != "") {
+                        m.channel.send(data.delivery.trim())
+                    }
+
+                    console.log(data)
+
+                })
+                .catch(() => {
+                    m.channel.send('API error')
+                })
+        }
+    }
 
     setTimeout(() => { })  // why don't i create a timer according to users  ? i'm lazy af don't wana think so hard BRAIN 404 just chill with every hour hehe 
 
 })
-c.login('')
+c.login('token')
